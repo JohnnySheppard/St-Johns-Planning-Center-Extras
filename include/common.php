0ab242fd5c6e3501a27ed889521378b36d36738d
Johnny Sheppard <mail@johnnysheppard.com>
2011-09-16 18:58:24 +0100

    Added function that hides the setting up of the request to get, put or post data to Planning Center Online. This will make it easier to request and set data within the rest of the site.

--- include/common.php
+++ include/common.php
@@ -1,6 +1,6 @@
 <?php
 
-$callback_url = "http://www.johnnysheppard.com/stjohns/include/complete.php";
+$callback_url = "http://www.johnnysheppard.com/stjohns/include/complete.php"; //this needs changing so it works out where the script is located. (Rather than having the complete address hard coded.)
 
 $debug = true;
 $request_token_endpoint = "https://www.planningcenteronline.com/oauth/request_token";
@@ -40,4 +40,38 @@ function run_curl($url, $method = 'GET', $headers = null, $postvals = null){
     
     return $response;
 }
+
+function get_pco_data($url,$method = "GET",$content = Null){
+	global $pco_key, $pco_secret, $user_access_token, $user_access_token_secret;
+
+	$test_consumer  = new OAuthConsumer($pco_key, $pco_secret, NULL);
+	$access_consumer = new OAuthConsumer($user_access_token, $user_access_token_secret, NULL);
+
+	// build and sign request
+	$request = OAuthRequest::from_consumer_and_token($test_consumer,
+	  $access_consumer, 
+	  $method,
+	  $url, 
+	  NULL);
+	$request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(),
+	  $test_consumer, 
+	  $access_consumer
+	);
+	
+	if (isset($content)){
+		//define request headers
+		$headers = array("Accept: application/xml");
+		$headers[] = $request->to_header();
+		$headers[] = "Content-type: application/xml";
+		$response = run_curl($url, $method, $headers, $content);
+	}
+	else {
+		// make GET request
+		$response = run_curl($request, $method);
+	}
+	
+	return $response;
+
+}
+
 ?>
